77505e19739475e1834a2572731c7cf4b950deb9
Daqing Feng <fengdaqing@hotmail.com>
2017-03-29 14:23:15 +0800

    Delete gitinit.bat

--- gitinit.bat
+++
@@ -1,6 +0,0 @@
-echo "# MongoRepository" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/DaqingFeng/MongoRepository.git
-git push -u origin master
